95fdffe6186f80b6bd07a6d8bf790290d8579a9f
giorgia-dit <giorgia.ditano@gmail.com>
2025-12-10 22:04:29 +0100

    UI refinements

--- town/forms.py
+++ town/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 class CustomAuthenticationForm(AuthenticationForm):
     error_messages = {
         "invalid_login": _(
-            "Oops! Forse non sei il proprietario di questa casa, o forse la tua passaporta è errata."
+            "Oops! Forse non sei il proprietario di questa casa, o forse la tua chiave è errata."
         ),
         "inactive": _("This account is inactive."),
     }
